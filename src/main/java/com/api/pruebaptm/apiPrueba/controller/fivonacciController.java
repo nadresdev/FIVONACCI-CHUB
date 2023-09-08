0093e5374b6765899ac7a1f2cac7bdf4ce3501cb
Weiner Andrés valencia gonzalez <nadresdev@gmail.com>
2023-09-08 11:56:20 -0500

    second commit

--- src/main/java/com/api/pruebaptm/apiPrueba/controller/fivonacciController.java
+++ src/main/java/com/api/pruebaptm/apiPrueba/controller/fivonacciController.java
@@ -11,7 +11,7 @@ import java.util.Collections;
 import java.util.List;
 
 @RestController
-@RequestMapping("/getfivonacci")
+@RequestMapping("/getfibonacci")
 public class fivonacciController {
 
 	 @GetMapping("/number/{index}")
